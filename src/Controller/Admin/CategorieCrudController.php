0d0c554ce089c60457209bccdcca8d6458e54642
pcpasapas <pcpasapas@gmail.com>
2023-01-22 01:22:50 +0100

    modified:   src/Controller/Admin/CategorieCrudController.php

--- src/Controller/Admin/CategorieCrudController.php
+++ src/Controller/Admin/CategorieCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Categorie;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
+
 class CategorieCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
