bf4748d00baa49d8e914e3044d03d2409396437b
pcpasapas <pcpasapas@gmail.com>
2023-01-22 01:22:47 +0100

    qqq

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Categorie;
 use App\Repository\CategorieRepository;
-use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
